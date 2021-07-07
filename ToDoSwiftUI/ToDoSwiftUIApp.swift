e659247266b9781c735a7d547275e37a0c6a542d
John Kostanov <kostanovd@icloud.com>
2021-07-07 13:46:15 +0200

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Джон Костанов on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
